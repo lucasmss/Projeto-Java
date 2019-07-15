32f747ab70d2e6ad2314f3c8516ea01dc1d3ee53
lucasmss16 <lucasmss16@gmail.com>
2019-07-15 16:43:56 -0300

    tela about

--- src/application/Main.java
+++ src/application/Main.java
@@ -9,6 +9,10 @@ import javafx.scene.control.ScrollPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
+	
 	public void start(Stage primaryStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainViw.fxml"));
@@ -19,7 +23,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
@@ -27,6 +31,12 @@ public class Main extends Application {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	public static Scene getMainScene() {
+		
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
