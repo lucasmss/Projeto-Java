69c6744adc2becb0ee5f7d5f50f4c222d74c11c8
lucasmss16 <lucasmss16@gmail.com>
2019-07-17 14:25:21 -0300

    update

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -9,12 +9,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable{
 
+	private Department entity;
+	
 	@FXML
 	private TextField textId;
-	
 	@FXML
 	private TextField textName;
 	@FXML
@@ -23,6 +25,14 @@ public class DepartmentFormController implements Initializable{
 	private Button btSave;
 	@FXML
 	private Button btCancel;
+	
+	
+	public void setDepartment(Department entity){
+		
+		this.entity = entity;
+		
+	}
+	
 	
 	@FXML
 	private void onBtSaveAction() {
@@ -49,6 +59,13 @@ public class DepartmentFormController implements Initializable{
 		
 	}
 	
-	
+	public void updateFormData(){
+		if (entity == null) {
+			throw new IllegalStateException("Entity não encontrado");
+		}
+		textId.setText(String.valueOf(entity.getId()));
+		textName.setText(entity.getName());
+		
+	}
 	
 }
