8b75715e0b81e79401b644eaf2090edf57a7a75f
lucasmss16 <lucasmss16@gmail.com>
2019-07-17 15:41:34 -0300

    update

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,17 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dbb.DbException;
+import gui.util.Alerts;
 import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable{
 
 	private Department entity;
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TextField textId;
@@ -33,13 +41,43 @@ public class DepartmentFormController implements Initializable{
 		
 	}
 	
+	public void setDepartmentService(DepartmentService service) {
+		
+		this.service =service;
+	}
+	
 	
 	@FXML
-	private void onBtSaveAction() {
+	private void onBtSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
 		
-		System.out.println("onBtSaveAction");
-		
+		}
+		catch(DbException e) {
+			
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
 	}
+	
+	private Department getFormData() {
+		
+		Department obj = new Department();
+		
+		obj.setId(Utils.tryParseToInt(textId.getText()));
+		obj.setName(textName.getText());
+		
+		return obj;
+	}
+	
+	
 	@FXML
 	private void onBtCancelAction() {
 	
--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -16,4 +16,18 @@ public class DepartmentService {
 
 		return dao.findAll();
 	}
+	
+	public void saveOrUpdate(Department obj){
+		
+		if(obj.getId() == null) {
+			
+			dao.insert(obj);
+		}
+		else {
+			dao.update(obj);
+		}
+		
+	}
+	
+	
 }
